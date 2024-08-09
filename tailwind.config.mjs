a4bc8f38b3787433152c08fb2f0811c526ee3e53
Jordi Yaputra <jordiyaputra@gmail.com>
2024-08-09 18:15:51 +0700

    setting layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
+
 export default {
   darkMode: ["class"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -66,7 +68,10 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        sans: ['"Noto Sans Variable"', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
