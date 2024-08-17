00cadf48d5bea2b553ba897e9ead25b6458ac7cd
Jordi Yaputra <jordiyaputra@gmail.com>
2024-08-17 17:34:03 +0700

    tika

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,7 +1,8 @@
 // keystatic.config.ts
-import { config, fields, collection } from "@keystatic/core";
+import { config, fields, collection, component } from "@keystatic/core";
 
 export default config({
+  ui: { brand: { name: "Paṭisambidha LAB" } },
   storage: {
     kind: "local",
   },
@@ -10,12 +11,41 @@ export default config({
       label: "Tika",
       slugField: "title",
       path: "src/content/tika/*",
-      // entryLayout: "content",
+      columns: ["title", "sentences"] as any[],
+      format: { contentField: "body" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        sentences: fields.array(fields.text({ label: "Sentence" }), {
+          label: "Sentences",
+          itemLabel: (props) => props.value,
+        }),
+        components: fields.blocks(
+          {
+            // First block option is a link to a Page
+            citta: {
+              label: "Citta",
+              itemLabel: (props) => props.value ?? "Citta",
+              schema: fields.relationship({
+                label: "Citta",
+                collection: "citta",
+              }),
+            },
+          },
+          { label: "Dhammas" }
+        ),
+        body: fields.markdoc({ label: "Body" }),
+      },
+    }),
+    citta: collection({
+      label: "Citta",
+      slugField: "name",
+      columns: ["name"],
+      path: "src/content/citta/*",
       format: {
         contentField: "content",
       },
       schema: {
-        title: fields.slug({ name: { label: "Title" } }),
+        name: fields.slug({ name: { label: "Name" } }),
         content: fields.markdoc({ label: "Content" }),
       },
     }),
