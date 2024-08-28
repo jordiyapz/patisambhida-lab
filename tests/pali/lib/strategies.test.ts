9f67ff689927d7378f4393850f03af0919b477d1
Jordi Yaputra <jordiyaputra@gmail.com>
2024-08-28 22:27:13 +0700

    WIP: add recursiveWindowStrategy

---
+++ tests/pali/lib/strategies.test.ts
@@ -0,0 +1,39 @@
+import {
+  recursiveWindowStrategy,
+  setNullStrategy,
+} from "@/modules/pali-translation/lib/strategies";
+import { initializeLines } from "@/modules/pali-translation/lib/utils";
+import { describe, expect, test } from "vitest";
+
+describe("Set null strategy", () => {
+  test("should return null", () => {
+    const res = setNullStrategy(
+      "In Lorem enim velit deserunt officia veniam tempor reprehenderit irure fugiat.",
+      []
+    );
+    expect(res).toBeNull();
+  });
+});
+
+describe("Recursive window strategy", () => {
+  test("should not change if nothing changes", () => {
+    const transcript =
+      "In Lorem enim velit, deserunt officia veniam. tempor reprehenderit irure fugiat.";
+    const lines = initializeLines(transcript);
+    const transformed = recursiveWindowStrategy(transcript, lines);
+    expect(transformed).toEqual(lines);
+  });
+  test("should change", () => {
+    const originalTranscript =
+      "In Lorem enim velit, deserunt officia veniam. tempor reprehenderit irure fugiat.";
+    const newTranscript =
+      "In Lorem enim velit, officia veniam. tempor reprehenderit irure fugiat.";
+    const originalLines = initializeLines(originalTranscript);
+    const newLines = initializeLines(newTranscript);
+
+    const transformed = recursiveWindowStrategy(newTranscript, originalLines);
+    expect(transformed).toEqual(newLines);
+  });
+  // TODO: should handle multiple lines
+  test.todo("should handle multiple lines");
+});
